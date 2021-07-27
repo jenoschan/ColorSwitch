dfc07dfc922d1f2d54f629f264ae1d4d329594e3
Jenny <you@example.com>
2021-07-27 19:11:43 -0400

    Level3

--- src/colorswitch/Level3.java
+++ src/colorswitch/Level3.java
@@ -8,10 +8,35 @@ public class Level3 extends Level{
         double x = screenWidth / 2;
 
         // Création des obstacles
-        Square obstacle1 = new Square(screenWidth / 2, screenHeight * 2, screenHeight * 1.9);
+        Square obstacle1 = new Square(x, 0.75 * screenHeight, 40);
+        Square obstacle2 = new Square(x, 2 * screenHeight, 60);
+        Square obstacle3 = new Square(x, 3 * screenHeight, 150);
+        Square obstacle4 = new Square(x, 3.6 * screenHeight, 200);
+        VerticalBar obstacle5 = new VerticalBar(x, 2.6 * screenHeight, 40, 100);
+        GrowingCircle obstacle6 = new GrowingCircle(x, 1.25 * screenHeight, 25);
+        RotatingCircle obstacle7 = new RotatingCircle(x, 4.2 * screenHeight, 30);
+        VerticalBar obstacle8 = new VerticalBar(x, 5.6 * screenHeight, 40, 200);
 
         obstacles.add(obstacle1);
+        obstacles.add(obstacle2);
+        obstacles.add(obstacle3);
+        obstacles.add(obstacle4);
+        obstacles.add(obstacle5);
+        obstacles.add(obstacle6);
+        obstacles.add(obstacle7);
+        obstacles.add(obstacle8);
 
-        victoryMushroom = new Mushroom(screenWidth / 2, 100 * screenHeight);
+        // Création des items
+        Potion potion1 = new Potion(x, 1 * screenHeight);
+        Potion potion2 = new Potion(x, 3.2 * screenHeight);
+        Potion potion3 = new Potion(x, 5 * screenHeight);
+
+        Shield shield = new Shield(x, 2.3* screenHeight);
+
+        items.add(potion1);
+        items.add(potion2);
+        items.add(shield);
+
+        victoryMushroom = new Mushroom(screenWidth / 2, 6 * screenHeight);
     }
 }
